2a00d60ae2da24bf03f6a5517d1021a8ffde3049
Cedric <cedric.servais@outlook.com>
2022-05-23 20:04:11 +0200

    Add isva_system_version module

---
+++ plugins/module_utils/isva_system_version.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright: (c) 2022, Cédric Servais <cedric.servais@outlook.com>
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ansible_collections.community.isva.plugins.module_utils.isva_utils import parse_fail_message
+from ansible_collections.community.isva.plugins.module_utils.common import ISVAModuleError
+from ansible.module_utils.connection import Connection
+
+import logging
+
+uri = '/core/sys/versions'
+
+logger = logging.getLogger(__name__)
+
+def fetch_system_version(module):
+    """ This function fetch the system version information from the appliance.
+
+    Returns:
+        _type_: _description_
+    """
+    connection = Connection(module._socket_path)
+    response = connection.send_request(path=uri)
+
+    if response['code'] != 200:
+        raise ISVAModuleError(parse_fail_message(response['code'], response['contents']))
+
+    return response['contents']
---
+++ plugins/modules/isva_system_version.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright: (c) 2022, Cédric Servais <cedric.servais@outlook.com>
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: isva_system_version
+short_description: Collect information about the First Steps Setup process.
+description:
+  - Collect information about the First Steps Setup process.
+version_added: "1.0.0"
+extends_documentation_fragment: community.isva.modules.isva
+author:
+  - Cédric Servais (@7893254)
+'''
+
+EXAMPLES = r'''
+- name: Collect ISVA First Steps status
+  isva_system_version:
+    state: gathered
+'''
+
+RETURN = r'''
+gathered:
+  description: A boolean indicating whether the service agreements have been accepted.
+  returned: queried
+  type: bool
+  sample: true
+'''
+
+import logging
+from io import StringIO
+
+from ansible.module_utils.basic import AnsibleModule
+
+from ansible_collections.community.isva.plugins.module_utils.isva_system_version import fetch_system_version
+
+from ansible_collections.community.isva.plugins.module_utils.isva_utils import (
+  create_return_object, create_return_error, setup_logging, update_logging_info
+)
+
+logger = logging.getLogger(__name__)
+str_log = StringIO()
+error_log = StringIO()
+
+class ArgumentSpec(object):
+  def __init__(self):
+    self.supports_check_mode = True
+    argument_spec = dict(
+      state=dict(type='str', required=True, choices=['gathered']),
+      log_level=dict(type='str', default='INFO', choices=['CRITICAL', 'FATAL', 'ERROR', 'WARN', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'])
+    )
+    self.argument_spec = {}
+    self.argument_spec.update(argument_spec)
+
+def __exec_gathered(module):
+  response = fetch_system_version(module=module)
+  return response
+
+def exec_module(module):
+  state = module.params['state']
+
+  if state == 'gathered':
+    response = __exec_gathered(module=module)
+    return {'gathered': response}
+
+  return {}
+
+def main():
+  spec = ArgumentSpec()
+  module = AnsibleModule(
+      argument_spec=spec.argument_spec,
+      supports_check_mode=spec.supports_check_mode
+  )
+  try:
+    setup_logging(str_log, module.params['log_level'])
+    logger.debug('Module parameters {}'.format(module.params))
+    
+    response = exec_module(module)
+    return_value = create_return_object()
+    update_logging_info(return_value, str_log.getvalue(), error_log.getvalue())
+    return_value.update(response)
+
+    module.exit_json(**return_value)
+  except Exception as e:
+    error_log.write(str(e))
+    error_log.write(str(e.code))
+    return_value = create_return_error(msg=str(e), stdout=str_log.getvalue(), stderr=error_log.getvalue())
+    module.fail_json(**return_value)
+
+if __name__ == '__main__':
+  main()
