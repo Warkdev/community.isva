8e8cd009917ff2041b60526f8b70c213b7e43a7a
Cedric <cedric.servais@outlook.com>
2022-05-28 15:50:48 +0200

    Add module to gather license facts

---
+++ plugins/module_utils/isva_licenses.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright: (c) 2022, Cédric Servais <cedric.servais@outlook.com>
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ansible_collections.community.isva.plugins.module_utils.isva_utils import parse_fail_message
+from ansible_collections.community.isva.plugins.module_utils.common import ISVAModuleError
+from ansible.module_utils.connection import Connection
+
+import json
+import logging
+
+uri = '/licenses'
+
+logger = logging.getLogger(__name__)
+
+
+def fetch_licenses(module):
+    """ This function fetch the licenses information from the appliance
+
+    Returns:
+        _type_: _description_
+    """
+    connection = Connection(module._socket_path)
+    response = connection.send_request(path=uri)
+
+    if response['code'] != 200:
+        raise ISVAModuleError(parse_fail_message(response['code'], response['contents']))
+
+    return response['contents']
